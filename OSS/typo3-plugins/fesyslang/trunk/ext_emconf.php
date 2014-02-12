c825b4b34b83176e587f3ca70e960ddb15588409
ralph <ralph@fdcf5c44-6d3c-11dc-8539-f79c8e7a170c>
2014-02-12 08:56:13 +0000

    typo3 compat 4.7

--- OSS/typo3-plugins/fesyslang/trunk/ext_emconf.php
+++ OSS/typo3-plugins/fesyslang/trunk/ext_emconf.php
@@ -30,6 +30,11 @@ $EM_CONF[$_EXTKEY] = Array (
 	'author_company' => '',
 	'private' => '',
 	'download_password' => '',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '4.5.0-4.7.99',
+		),
+	),
 	'version' => '1.0.0',	// Don't modify this! Managed automatically during upload to repository.
 	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"83b8";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"beea";s:14:"ext_tables.php";s:4:"bbb8";s:14:"ext_tables.sql";s:4:"a45f";s:24:"ext_typoscript_setup.txt";s:4:"c4e6";s:16:"locallang_db.php";s:4:"9ca8";s:14:"doc/manual.odt";s:4:"7c6e";s:14:"doc/manual.sxw";s:4:"7e34";s:19:"doc/wizard_form.dat";s:4:"0b8b";s:20:"doc/wizard_form.html";s:4:"2ed5";s:30:"pi1/class.tx_fesyslang_pi1.php";s:4:"cc2a";}',
 );
